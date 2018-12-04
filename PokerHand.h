0daba1636d443fd9dd347c23eb9e27c23dabc357
Abstract <31081170+JustenPhillips@users.noreply.github.com>
2018-12-04 02:40:08 -0800

    Create PokerHand.h

---
+++ PokerHand.h
@@ -0,0 +1,16 @@
+
+// Name: Justen Phillips
+// Course: CIS 006 Intro to programming
+// CRN: 20577
+// Date: 3/10/2017
+// Assignment: Poker Hand
+#ifndef PokerHand
+#define PokerHand
+#include <iostream>
+
+using namespace std;
+
+int checkHand(int [], const short);
+
+#endif
+ 
